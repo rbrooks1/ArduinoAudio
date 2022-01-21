0a9b76f15368faee7057937b88f0558886ef8c16
Ryan Brooks <rbrook1598@gmail.com>
2022-01-21 14:19:19 -0700

    open and find mp3 files in folder

--- Server.cpp
+++ Server.cpp
@@ -3,10 +3,27 @@
 #include<iostream>
 #include<stdio.h>
 #include<string.h>
+#include<fstream>
 #include"Nav.h"
 #include"ReadAudio.h"
 
 using namespace std;
+
+void findMp3 (vector<string> &files) {
+	system("ls > lspipe.txt");
+	ifstream file;
+	file.open("lspipe.txt", fstream::in);
+	
+	string fileName;
+	while (getline(file, fileName)) {
+		if (fileName.find(".mp3") != string::npos) {
+			files.push_back(fileName);
+		}
+	}
+
+	file.close();
+
+}
 
 int main() {
 	string dir;
@@ -15,6 +32,12 @@ int main() {
 	getline(cin, dir);
 
 	goToDir(dir);
-	
+
+	// find mp3 files to play on arduino
+	vector<string> files;
+	system("touch lspipe.txt");
+	findMp3(files);
+	remove("lspipe.txt");
+
 	return 0;
 }
